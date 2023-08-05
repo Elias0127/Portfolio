700725483e274f8e52b609b1943e4754e23b1500
Elias Woldie <EliasMekonen001@gmail.com>
2023-08-05 08:08:05 -0700

    Fixed naming issue with Education component

--- src/components/index.js
+++ src/components/index.js
@@ -4,7 +4,7 @@ import Navbar from './Navbar';
 import About from './About';
 import Tech from './Tech';
 import Experience from './Experience';
-import Education from './Educate';
+import Education from './Education';
 import Works from './Works';
 import OpenSource from './OpenSource';
 import Feedbacks from './Feedbacks';
